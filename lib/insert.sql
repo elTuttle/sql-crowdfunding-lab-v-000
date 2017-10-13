feb9d4b489582d4e800d0532702d2dc4e2f9e900
John Tuttle <thejwt@gmail.com>
2017-10-13 20:57:44 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,20 @@
+INSERT INTO projects(title,category,funding_goal,start_date,end_date)
+VALUES("art project","art",20000,"7/20/2017","8/20/2017");
+INSERT INTO projects(title,category,funding_goal,start_date,end_date)
+VALUES("comic project","comics",10000,"6/25/2017","9/30/2017");
+INSERT INTO projects(title,category,funding_goal,start_date,end_date)
+VALUES("dance project","dance",1500,"7/23/2017","10/20/2017");
+INSERT INTO projects(title,category,funding_goal,start_date,end_date)
+VALUES("design project","design",30000,"5/10/2017","6/29/2017");
+INSERT INTO projects(title,category,funding_goal,start_date,end_date)
+VALUES("art project 2","art",40000,"9/5/2017","12/11/2017");
+INSERT INTO projects(title,category,funding_goal,start_date,end_date)
+VALUES("film project","film",20000,"7/20/2017","8/20/2017");
+INSERT INTO projects(title,category,funding_goal,start_date,end_date)
+VALUES("food project","food",500,"2/10/2017","3/10/2017");
+INSERT INTO projects(title,category,funding_goal,start_date,end_date)
+VALUES("music project","music",3000,"12/15/2017","1/20/2018");
+INSERT INTO projects(title,category,funding_goal,start_date,end_date)
+VALUES("photography project","photography",2500,"6/20/2017","8/30/2017");
+INSERT INTO projects(title,category,funding_goal,start_date,end_date)
+VALUES("art project 3","art",750,"1/23/2017","3/4/2017");
